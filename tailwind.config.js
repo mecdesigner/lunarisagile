02194044976593bfc2154c1764290a46a6a37a96
mecdesigner <info@mecdesigner.com>
2025-06-06 14:42:41 +0200

    New content and new template changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 module.exports = {
-    content: ["./*.html"],
-    theme: {
-      extend: {
-        colors: {
-          primary: '#e63746',
-        }
-      }
-    }
-  }
-  
+  darkMode: 'class',
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#e63746',     // CTA, highlight
+        bg: '#f2faef',          // Main background
+        accent: '#a7dadc',      // Accent
+        accentdark: '#447a9c',  // Accent dark
+        basedark: '#1d3658',    // Main dark
+      },
+    },
+  },
+};
